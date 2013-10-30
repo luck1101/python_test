51517c53edf08ed3174250c159b10f30f73288a7
smilexiao1101 <1101546153@qq.com>
2013-10-31 00:14:46 +0800

    change multicast client
    change multicast client

--- sample/multicast/client.py
+++ sample/multicast/client.py
@@ -13,6 +13,8 @@ def main():
 	mreq = struct.pack("=4sl",socket.inet_aton(MULTICAST_GRP),socket.INADDR_ANY)
 	s.setsockopt(socket.IPPROTO_IP,socket.IP_ADD_MEMBERSHIP,mreq)
 	while 1:
+		print "recv wait............."
 		print "recvied from server" + s.recv(1024)
+
 if __name__ == "__main__":
 	main()
